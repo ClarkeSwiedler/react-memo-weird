3c5d80a1d1c6c47428da278a4e31b8eb7001e295
Clarke Swiedler <Clarke@swiedler.com>
2020-01-26 21:44:03 -0500

    Reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState, memo } from "react";
+
+const testStrings: string[] = ["One", "Two", "Three", "Four", "Five"];
+
+interface ListItemProps {
+  selected: boolean;
+  item: string;
+  onSelected: (item: string) => void;
+}
+
+const ListItem: FC<ListItemProps> = memo(
+  props => {
+    return (
+      <div
+        onClick={() => props.onSelected(props.item)}
+        style={{
+          backgroundColor: `${props.selected ? "darkred" : "darkblue"}`,
+          width: "100px",
+          height: "65px",
+          color: "white"
+        }}
+      >
+        {props.item}
+      </div>
+    );
+  },
+  propsAreEqual
+);
+
+function propsAreEqual(prev: ListItemProps, next: ListItemProps) {
+  return prev.selected === next.selected;
+  // return false;
+}
+
+const SelectableList = () => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const handleItemSelection = (item: string) => {
+    if (selectedItems.includes(item)) {
+      // setSelectedItems(selectedItems.filter(name => name !== item));
+      setSelectedItems(selectedItems => selectedItems.filter(name => name !== item));
+    } else {
+      // setSelectedItems([...selectedItems, item]);
+      setSelectedItems(selectedItems => [...selectedItems, item]);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      {testStrings.map(item => (
+        <ListItem
+          onSelected={handleItemSelection}
+          selected={selectedItems.includes(item)}
+          item={item}
+        />
+      ))}
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SelectableList />
     </div>
   );
-}
+};
 
 export default App;
